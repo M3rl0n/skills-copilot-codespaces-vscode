aa9e6638b81663ca1e5ec51ef9831139e8652bd0
MarkGs <141935744+M3rl0n@users.noreply.github.com>
2024-12-05 15:27:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -3,6 +3,24 @@
 // create a new comment object and add it to the comments array
 // send back a response that contains the new comment object
 // start the server
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+const comments = [];
+
+app.use(bodyParser.json());
+
+app.post('/comments', (req, res) => {
+  const newComment = req.body;
+  comments.push(newComment);
+  res.status(201).json(newComment);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 
 
